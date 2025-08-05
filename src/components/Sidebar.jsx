6c431befc383ef181c1604ebb6b976c2fadfa93e
soylltari <mmmsribranets@gmail.com>
2025-08-05 17:23:20 +0300

    feat: add sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+export default function Sidebar() {
+  return (
+    <nav className="md:hidden fixed top-0 right-0 z-50 h-screen w-1/2 px-4 py-4 bg-gradient-to-b from-blue-300 to-blue-400 backdrop-blur-sm shadow-xl">
+      <ul className="flex flex-col gap-6 mt-8 font-bold text-xl text-gray-50 [&>li]:py-2 [&>li]:px-2">
+        <li>Home</li>
+        <li>Library</li>
+        <li>Quiz</li>
+      </ul>
+    </nav>
+  );
+}
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export default function MainLayout() {
   return (
     <div>
       <Navbar />
+      <Sidebar />
       <Outlet />
     </div>
   );
