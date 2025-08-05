43626b535cec8aa6e6f6178c702f7322b0dee560
soylltari <mmmsribranets@gmail.com>
2025-08-05 14:05:10 +0300

    feat: add routing to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,19 @@
 import { IoMenu } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="flex justify-end sticky top-0 z-50">
-      <ul className="hidden md:flex gap-12 [&>li]:cursor-pointer">
-        <li>Home</li>
-        <li>Library</li>
-        <li>Quiz</li>
+      <ul className="hidden md:flex gap-12">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/library">Library</Link>
+        </li>
+        <li>
+          <Link to="/quiz">Quiz</Link>
+        </li>
       </ul>
       <button className="md:hidden">
         <IoMenu size={40} />
