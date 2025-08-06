0de1acb90cf6bfbbe1731c31449d0ead0c77b638
soylltari <mmmsribranets@gmail.com>
2025-08-06 13:47:22 +0300

    stye: add slide animation to sidebar and hover effects to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Navbar({ onMenuClick }) {
   return (
     <nav className="flex justify-end sticky top-0 z-50 py-4">
-      <ul className="hidden md:flex gap-12">
+      <ul className="hidden md:flex gap-12 [&>li]:hover:text-blue-500 [&>li]:transition-colors">
         <li>
           <Link to="/">Home</Link>
         </li>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -16,11 +16,10 @@ export default function Sidebar({ isOpen, onClose }) {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, [isOpen, onClose]);
 
-  if (!isOpen) return null;
   return (
     <nav
       id="sidebar"
-      className={`md:hidden fixed top-0 right-0 z-50 h-screen w-1/2 px-4 py-4 bg-gradient-to-b from-blue-300 to-blue-400 backdrop-blur-sm shadow-xl ${
+      className={`md:hidden fixed top-0 right-0 z-50 h-screen w-1/2 px-4 py-4 bg-gradient-to-b from-blue-300 to-blue-400 backdrop-blur-sm shadow-xl transform transition-transform duration-300 ease-in-out ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
