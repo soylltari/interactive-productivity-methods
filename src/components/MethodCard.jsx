fe48e1f9293dbbff7ee04b118d68741aad545abf
soylltari <mmmsribranets@gmail.com>
2025-08-19 19:30:57 +0300

    feat: add MethodCard component

---
+++ src/components/MethodCard.jsx
@@ -0,0 +1,29 @@
+export default function MethodCard({ method }) {
+  return (
+    <div className="main-gradient p-[2px] rounded-3xl w-80 hover:shadow-[0px_4px_14px_0_rgba(147,197,253,.70)] transition-all">
+      <div className="bg-gray-50 rounded-[1.4rem] cursor-pointer w-full min-h-86 flex flex-col">
+        <div className="h-48 flex items-center justify-center p-4">
+          <img
+            src={method.icon}
+            alt={method.id}
+            className="h-32 w-32 object-contain"
+          />
+        </div>
+        <div className="w-full h-[2px] bg-gray-200"></div>
+        <div className="px-6 py-6 text-left flex-1">
+          <h2>{method.name}</h2>
+          <div className="flex flex-wrap mt-4 gap-2">
+            {method.tags.map((tag, id) => (
+              <p
+                key={id}
+                className="capitalize text-sm bg-blue-300 rounded-full px-2"
+              >
+                {tag}
+              </p>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
