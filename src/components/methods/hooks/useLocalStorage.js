e5ddab633ced529575275e8af502ed9b60cece4f
soylltari <mmmsribranets@gmail.com>
2025-09-23 17:58:20 +0300

    fix: correct window type checks and improve localStorage handling in useLocalStorage hook

--- src/components/methods/hooks/useLocalStorage.js
+++ src/components/methods/hooks/useLocalStorage.js
@@ -4,10 +4,10 @@ export function useLocalStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState(() => {
     let currentValue;
     try {
-      if (window === "undefined") return initialValue;
+      if (typeof window === "undefined") return initialValue;
 
       const item = window.localStorage.getItem(key);
-      currentValue = JSON.parse(item) || initialValue;
+      currentValue = item ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.log(error);
       currentValue = initialValue;
@@ -18,8 +18,8 @@ export function useLocalStorage(key, initialValue) {
   const setValue = (value) => {
     try {
       setStoredValue(value);
-      if (window !== "undefined")
-        return window.localStorage.setItem(key, JSON.stringify(value));
+      if (typeof window !== "undefined")
+        window.localStorage.setItem(key, JSON.stringify(value));
     } catch (error) {
       console.log(error);
     }
@@ -35,10 +35,12 @@ export function useLocalStorage(key, initialValue) {
         }
       }
     };
-    if (window !== "undefined") {
+
+    if (typeof window !== "undefined") {
       window.addEventListener("storage", handleStorageChange);
-      return window.removeEventListener("storage", handleStorageChange);
+      return () => window.removeEventListener("storage", handleStorageChange);
     }
   }, [key]);
+
   return [storedValue, setValue];
 }
